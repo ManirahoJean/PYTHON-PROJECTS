a70bf35468ceb86f7a41ea5602cfd37a37f042a1
Maniraho <johmaniraho42@gmail.com>
2023-03-21 09:21:42 +0200

    Add

--- 1.VARIABLES AND DATA TYPES/Variables/hello.py
+++ 1.VARIABLES AND DATA TYPES/Variables/hello.py
@@ -1,1 +1,13 @@
+# This a comment in python
+# the following function print "Hello world python"
+
+print("Hello world python")
+
+# Variable is connected to a value which is the information associated with that variable
+# message is a variable
+
+message = "Hello world python"
+print(message)
+
+
 
