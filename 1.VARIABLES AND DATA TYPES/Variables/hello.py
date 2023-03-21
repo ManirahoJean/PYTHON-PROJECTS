7af3ee932d489bd3388464fbc377fdca137ac04e
Maniraho <johmaniraho42@gmail.com>
2023-03-21 22:49:10 +0200

    Add variable error avoidance

--- 1.VARIABLES AND DATA TYPES/Variables/hello.py
+++ 1.VARIABLES AND DATA TYPES/Variables/hello.py
@@ -1,13 +1,24 @@
-# This a comment in python
-# the following function print "Hello world python"
+# This is a comment in python
+# the following  prints "Hello world python"
 
 print("Hello world python")
 
 # Variable is connected to a value which is the information associated with that variable
+# Variables are often described as boxes you can store values in
 # message is a variable
 
 message = "Hello world python"
 print(message)
+greeting = "hi there!"
+print(greeting)
+
+# avoid errors in variable
+""""" 
+message = "python says hello to the world"
+print(mesage)
+
+"""
+
 
 
 
