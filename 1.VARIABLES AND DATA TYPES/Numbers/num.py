0f3c2b5b95064c176a5b258038bcc35b9e644dbd
Maniraho <johmaniraho42@gmail.com>
2023-03-23 21:19:15 +0200

    Add arithmetic operations, multiple assignment

--- 1.VARIABLES AND DATA TYPES/Numbers/num.py
+++ 1.VARIABLES AND DATA TYPES/Numbers/num.py
@@ -1,1 +1,30 @@
 
+# Integers
+addition = 4+5
+print(addition)
+subs = 5-3
+print(subs)
+mult = 2*3
+print(mult)
+divide = 6/2
+print(divide)
+
+power = 3**2
+print(power)
+
+# Floats
+addition2 = 2.1 + 3.2
+
+# Underscores in Numbers
+# when you use long numbers you can use underscore
+
+royal_age = 30_000_000_000
+print(royal_age)
+
+# Multiple assignment
+x, y, z = 10, 12, 15
+
+# Constant is like a variable whose value remains the same through the
+# entire project
+
+const_max = 500
