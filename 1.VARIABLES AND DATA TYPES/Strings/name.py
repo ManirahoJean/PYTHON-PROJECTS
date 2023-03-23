a612f27881804adc532b0c34a34e5676fdf84df8
Maniraho <johmaniraho42@gmail.com>
2023-03-23 11:21:52 +0200

    Change Case in a string

--- 1.VARIABLES AND DATA TYPES/Strings/name.py
+++ 1.VARIABLES AND DATA TYPES/Strings/name.py
@@ -1,1 +1,22 @@
+# A string is a series of characters
+
+"This is a string in python" # this is a string
+'This is also another string in python'
+
+# Using quotes and apostrophes within strings:
+
+'I read, "Python is one of the powerful languages"'
+"One of Python's strengths is its diverse and supportive community."
+
+# Changing Case in a String with Methods
+
+name = "python language"
+print(name.title())
+
+second_name = "Always Learning"
+print(second_name.lower())
+print(second_name.upper())
+
+
+
 
