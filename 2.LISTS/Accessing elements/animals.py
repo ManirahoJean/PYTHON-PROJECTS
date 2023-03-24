ded3db5cca9fe6688e1b17326f6f6ea9d600083f
Maniraho <johmaniraho42@gmail.com>
2023-03-24 16:58:51 +0200

    Access elements in a list

--- 2.LISTS/Accessing elements/animals.py
+++ 2.LISTS/Accessing elements/animals.py
@@ -1,1 +1,22 @@
+# A list is a collection of items in a particular order
+
+animal = ['rabbit', 'cok', 'cat', 'dog']
+print(animal)
+
+# index positions starts from 0, not 1
+
+print(animal[0])
+print(animal[0].title())
+
+# Accessing the last element in python
+
+print(animal[-1])
+
+message = f"My first animal was {animal[0].title()} !"
+print(message)
+
+
+
+
+
 
