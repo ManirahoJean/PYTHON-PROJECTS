e19c99a27577be2741efc76f952d9f70d0e868a3
Maniraho <johmaniraho42@gmail.com>
2023-03-26 09:43:50 +0200

    Sort element in a list
    Find length of a list

--- 2.LISTS/Organizing List/clothes.py
+++ 2.LISTS/Organizing List/clothes.py
@@ -1,1 +1,32 @@
+# Sorting a List Permanently with the sort() Method
+
+clothes = ['shirt', ' coat', 't-shirt', 'jumper']
+clothes.sort()
+print(clothes)
+
+# Sort this list in reverse alphabetical order
+
+clothes.sort(reverse=True)
+print(clothes)
+
+cars = ['toyota', 'bmw', 'udi', 'subaru']
+print(f"Here is the original list:\n{cars}")
+print(f"Here is the sorted list: \n{sorted(cars)}")
+
+# Printing a List in Reverse Order
+cars.reverse()
+print(cars)
+
+# Finding the length of the list
+
+print(len(cars))
+
+
+
+
+
+
+
+
+
 
