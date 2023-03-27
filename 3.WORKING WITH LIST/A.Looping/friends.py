2832647e8b1567adcfe90f63974fb734fdb55cde
Maniraho <johmaniraho42@gmail.com>
2023-03-27 08:46:34 +0200

    Add for loop

--- 3.WORKING WITH LIST/A.Looping/friends.py
+++ 3.WORKING WITH LIST/A.Looping/friends.py
@@ -1,1 +1,13 @@
 
+# Looping to print every friend in friends
+
+friends = ['rukundo', 'aimable', 'juru']
+for friend in friends:
+    print(friend)
+    print(F"{friend.title()}, was awesome!\n")
+    print(F"I can't wait to see your next show, {friend.title()}.")
+print("Thank you everyone, this was the greatest show")
+
+
+
+
