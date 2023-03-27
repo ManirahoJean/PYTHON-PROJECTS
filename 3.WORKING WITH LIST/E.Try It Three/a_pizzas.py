dcc22679ea0b5b01eda8cb2c9e9f2a5b2ba0cc77
Maniraho <johmaniraho42@gmail.com>
2023-03-27 19:37:16 +0200

    Add for loop

--- 3.WORKING WITH LIST/E.Try It Three/a_pizzas.py
+++ 3.WORKING WITH LIST/E.Try It Three/a_pizzas.py
@@ -1,1 +1,8 @@
+# 3.1 Pizza
+
+pizzas = ['cheese', 'meat', 'veggie', 'pepperoni']
+for pizza in pizzas:
+    print(pizza)
+    print(F"I like {pizza.title()} pizza\n")
+print(f"I really love pizza!")
 
