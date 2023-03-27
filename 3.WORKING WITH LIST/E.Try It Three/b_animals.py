dfb5a17c55a3e3b22e0b88ef9be16a70c2d207f7
Maniraho <johmaniraho42@gmail.com>
2023-03-27 19:48:16 +0200

    Add for loop

---
+++ 3.WORKING WITH LIST/E.Try It Three/b_animals.py
@@ -0,0 +1,16 @@
+# 3.2 print animals list
+# Print animals name
+
+animals = ['lion', 'tiger', 'leopard']
+for animal in animals:
+    print(animal.title())
+
+print("\n")
+
+# print a sentence about each animal
+
+for animal in animals:
+    print(f"a {animal.title()} is the great hunter")
+print("Any of these animals would have a great pet")
+
+
