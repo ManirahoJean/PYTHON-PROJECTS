b9b678df3298ef845e57626f5937d1efb6d78e91
Maniraho <johmaniraho42@gmail.com>
2023-03-29 10:58:11 +0200

    Avoid errors in variable

--- 1.VARIABLES AND DATA TYPES/Variables/hello.py
+++ 1.VARIABLES AND DATA TYPES/Variables/hello.py
@@ -7,13 +7,13 @@ print("Hello world python")
 # Variables are often described as boxes you can store values in
 # message is a variable
 
-message = "Hello world python"
+message = "Hello world python !"
 print(message)
 greeting = "hi there!"
 print(greeting)
 
 # avoid errors in variable
-""""" 
+"""""
 message = "python says hello to the world"
 print(mesage)
 
