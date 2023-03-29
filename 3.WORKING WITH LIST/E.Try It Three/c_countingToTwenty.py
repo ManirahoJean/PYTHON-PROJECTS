a3ae6aa362200965ee5a15380efd374a9a94c3a3
Maniraho <johmaniraho42@gmail.com>
2023-03-29 10:40:22 +0200

    Count to twenty

---
+++ 3.WORKING WITH LIST/E.Try It Three/c_countingToTwenty.py
@@ -0,0 +1,22 @@
+# 3.3 Counting to twenty
+
+countToTwenty = range(1, 21)
+for value in countToTwenty:
+    print(value)
+
+print("\n")
+
+new_countToTwenty = list(range(1, 21))
+for new_value in new_countToTwenty:
+    print(new_value)
+
+print("\n")
+
+numbers = []
+for number in range(1, 21):
+    numbers.append(number)
+print(numbers)
+
+
+
+
