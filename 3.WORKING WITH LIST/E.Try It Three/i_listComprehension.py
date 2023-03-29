b3eedf789d72233ba107ad6113ebb6c1338131cf
Maniraho <johmaniraho42@gmail.com>
2023-03-29 10:52:37 +0200

    Add list comprehension

---
+++ 3.WORKING WITH LIST/E.Try It Three/i_listComprehension.py
@@ -0,0 +1,11 @@
+# Cube Comprehension
+
+myList = [value**3 for value in range(1, 11)]
+print(myList)
+
+print("\n")
+
+# This works too
+myList = [value**3 for value in range(1, 11)]
+for new_list in myList:
+    print(new_list)
