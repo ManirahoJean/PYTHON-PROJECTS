5e7bdb2eb1a6ce0adf468c963ca374b41e2f6773
Maniraho <johmaniraho42@gmail.com>
2023-03-30 12:14:09 +0200

    Slice a list

---
+++ 3.WORKING WITH LIST/E.Try It Three/j_ slices.py
@@ -0,0 +1,18 @@
+# 3.10 add several lines
+
+my_favorite_foods = ['vegetables', 'fruits', 'meat pizza', 'african meat', 'beans']
+
+print("My first favorite foods are:")
+print(my_favorite_foods[0:3])
+
+print("\nMy favorite foods from the middle are:")
+print(my_favorite_foods[2:])
+
+print("\nThe last favorite foods are:")
+print(my_favorite_foods[-2:])
+
+
+
+
+
+
