ea421c94426e624430693bc3320d8a6e0aab4186
Maniraho <johmaniraho42@gmail.com>
2023-03-30 23:51:13 +0200

    Check value in a list

--- 4.IF STATEMENT/A.Conditional Test/cars.py
+++ 4.IF STATEMENT/A.Conditional Test/cars.py
@@ -1,1 +1,29 @@
 
+cars = ['audi', 'subaru', 'bmw', 'toyota']
+
+for car in cars:
+    if car == 'bmw':
+        print(car.upper())
+    else:
+        print(car.title())
+
+# Checking for Inequality
+
+requested_toppings = 'mushrooms'
+if requested_toppings != 'anchovies':
+    print("hold the anchovies")
+
+# magic number
+
+answer = 17
+if answer != 42:
+    print("That is not the correct answer, please try again!")
+
+# Checking Whether a Value Is Not in a List
+
+banned_users = ['david', 'charles', 'carolina']
+user = 'marie'
+if user not in banned_users:
+    print(f"{user.title()}, you can post a response if you wish!")
+
+
