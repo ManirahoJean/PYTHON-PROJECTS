1048183eaab37b22169b1eace535ee793a2f85c8
Maniraho <johmaniraho42@gmail.com>
2023-03-30 12:41:59 +0200

    Redefine a tuple

--- 3.WORKING WITH LIST/D.Tuples/dimensions.py
+++ 3.WORKING WITH LIST/D.Tuples/dimensions.py
@@ -1,1 +1,26 @@
+# Tuples refers to the values that cannot be changed
+#  and an immutable list is called a tuple.
 
+dimensions = (200, 50)
+
+print(dimensions[0])
+print(dimensions[1])
+
+# dimensions[0] = 300 this is the error
+
+print(dimensions)
+
+# Looping Through All Values in a Tuple
+
+print("\nOriginal dimensions are: \n")
+
+for dimension in dimensions:
+    print(dimension)
+
+
+dimensions = (400, 200)
+
+print("\nModified dimensions are: \n")
+
+for dimension in dimensions:
+    print(dimension)
