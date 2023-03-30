4f551b97fc5fdac8c80c809f8ce137759a73e4cd
Maniraho <johmaniraho42@gmail.com>
2023-03-30 12:54:58 +0200

    Redefine a tuple

---
+++ 3.WORKING WITH LIST/E.Try It Three/l_buffet.py
@@ -0,0 +1,15 @@
+# 3.12 redefine a tuple
+
+buffet = ('meat', 'chips', 'rice', 'potatoes', 'beans')
+
+print("Our restaurant offers the following foods: \n")
+
+for food in buffet:
+    print(food)
+
+buffet = ('meat', 'irish potatoes', 'rice', 'potatoes', 'beans')
+
+print("\nOur new foods are the following: \n")
+
+for food in buffet:
+    print(food)
