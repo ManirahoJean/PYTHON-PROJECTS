87deee8fb8c0a2b821c087ff276e67b1e732e109
Maniraho <johmaniraho42@gmail.com>
2023-03-31 10:35:41 +0200

    Add elif blocks

--- 4.IF STATEMENT/E.Multiple elif Blocks/admission.py
+++ 4.IF STATEMENT/E.Multiple elif Blocks/admission.py
@@ -1,1 +1,10 @@
-
+age = 12
+if age < 4:
+    price = 0
+elif age < 18:
+    price = 100_000
+elif age < 65:
+    price = 400_000
+else:
+    price = 200_00
+print(f"Your admission cost is {price} Rwf")
