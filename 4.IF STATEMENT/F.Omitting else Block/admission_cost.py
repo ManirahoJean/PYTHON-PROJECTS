e9e1ce739e5e35446357f5248386ec7b7f7f51af
Maniraho <johmaniraho42@gmail.com>
2023-03-31 11:38:20 +0200

    Omit else block

--- 4.IF STATEMENT/F.Omitting else Block/admission_cost.py
+++ 4.IF STATEMENT/F.Omitting else Block/admission_cost.py
@@ -1,1 +1,10 @@
-
+age = 12
+if age < 4:
+    price = 0
+elif age < 18:
+    price = 100000
+elif age < 65:
+    price = 400000
+elif age >= 65:
+    price = 20
+print(f"Your admission cost is {price} Rwf")
