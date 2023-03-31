842111495f3e09eb736786c22e65bd9df62b100f
Maniraho <johmaniraho42@gmail.com>
2023-03-31 08:49:57 +0200

    Add if-elif-else chain

--- 4.IF STATEMENT/D.If-elif-else Chain/amusement_park.py
+++ 4.IF STATEMENT/D.If-elif-else Chain/amusement_park.py
@@ -1,1 +1,21 @@
+age = 12
+if age < 4:
+    print("Your admission cost is 0 Rwf")
+elif age < 18:
+    print("Your admission cost is 100,000 Rwf")
+else:
+    print("Your admission is 40, 000 Rwf cost")
 
+print("\n")
+
+# set the price inside the if-elif-else chain
+
+age_1 = 12
+if age_1 < 4:
+    price = 0
+elif age_1 < 18:
+    price = 100_000
+else:
+    price = 40_000
+
+print(f"Your admission is {price} Rwf cost")
