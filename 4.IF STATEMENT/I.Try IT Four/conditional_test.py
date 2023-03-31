e66b30e84aec4d6cea81edf7ee40caecb90028dd
Maniraho <johmaniraho42@gmail.com>
2023-03-31 08:20:09 +0200

    Check conditional test

--- 4.IF STATEMENT/I.Try IT Four/conditional_test.py
+++ 4.IF STATEMENT/I.Try IT Four/conditional_test.py
@@ -1,1 +1,8 @@
+# 4.1 Conditional test
 
+car = 'toyota'
+print("Is car =='toyota' ? I predict True.")
+print(car == 'toyota')
+
+print("\nIs car == 'saburo' ? I predict False")
+print(car == 'saburo')
