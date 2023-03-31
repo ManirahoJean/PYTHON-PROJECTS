b4ffb648cc6f5d73eda3dc62b5e8ed1ac53097e2
Maniraho <johmaniraho42@gmail.com>
2023-03-31 08:16:12 +0200

    Check value in a list or not

--- 4.IF STATEMENT/A.Conditional Test/cars.py
+++ 4.IF STATEMENT/A.Conditional Test/cars.py
@@ -1,4 +1,4 @@
-
+# Simple conditional statement
 cars = ['audi', 'subaru', 'bmw', 'toyota']
 
 for car in cars:
@@ -7,17 +7,62 @@ for car in cars:
     else:
         print(car.title())
 
+print("\n")
+
+# Checking for Equality
+
+car = 'bmw'
+print(car == 'bmw')  # it prints True
+print(car == 'audi')  # it prints False
+
+print("\n")
+
 # Checking for Inequality
 
 requested_toppings = 'mushrooms'
 if requested_toppings != 'anchovies':
     print("hold the anchovies")
 
-# magic number
+print("\n")
+
+# Numerical Comparisons
+
+age = 18
+print(age == 18)  # it prints True
 
 answer = 17
 if answer != 42:
     print("That is not the correct answer, please try again!")
+
+print("\n")
+
+# Using and to Check Multiple Conditions
+
+age_0 = 22
+age_1 = 18
+print((age_0 >= 21) and (age_1 >= 21))  # False, one of the conditions is false
+age_1 = 22
+print((age_0 >= 22) and (age_1 >= 22))  # True, both conditions are true
+
+print("\n")
+
+# Using or to Check Multiple Conditions
+
+age_0 = 22
+age_1 = 18
+print((age_0 >= 21) or (age_1 >= 21))  # True, True if one of the conditions is true
+age_0 = 18
+print((age_0 >= 21) or (age_1 >= 21))  # False, false if both conditions fail
+
+print("\n")
+
+# Checking Whether a Value Is in a List
+
+requested_topping_1 = ['mushrooms', 'onions', 'pineapple']
+print('mushrooms' in requested_topping_1)  # it prints yes
+print('pepperoni' in requested_topping_1)
+
+print("\n")
 
 # Checking Whether a Value Is Not in a List
 
