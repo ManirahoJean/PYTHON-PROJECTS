513ffe84467ed5e8447e7acb1e611db600c69896
Maniraho <johmaniraho42@gmail.com>
2023-03-31 08:35:55 +0200

    Add if else

--- 4.IF STATEMENT/C.If Else Statement/voting_age.py
+++ 4.IF STATEMENT/C.If Else Statement/voting_age.py
@@ -1,1 +1,8 @@
+age = 17
+if age >= 18:
+    print("You are old enough")
+    print("Have you registered to vote yet?")
+else:
+    print("Sorry, You are too young to vote.")
+    print("Please register to vote as soon as possible")
 
