d1e48535faeba104f82b320873c291d3d3ff8366
Maniraho <johmaniraho42@gmail.com>
2023-04-01 20:59:03 +0200

    Add failing version

--- 4.IF STATEMENT/I.Try IT Four/c_alien_colors.py
+++ 4.IF STATEMENT/I.Try IT Four/c_alien_colors.py
@@ -1,1 +1,15 @@
+# 4.3 alien color
+alien_color = ['green', 'yellow', 'red']
+if 'green' in alien_color:
+    print("You earned 5 points!")
 
+print("\n")
+
+if 'yellow' in alien_color:
+    print("\nYou earned 5 points")
+
+# failing version
+# there will be no output
+
+if 'white' in alien_color:
+    print("You earned 5 points")
