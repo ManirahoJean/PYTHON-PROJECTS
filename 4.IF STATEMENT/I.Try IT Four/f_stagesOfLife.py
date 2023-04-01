d103b95a7a14670f22d1e2e38d67517505cbe534
Maniraho <johmaniraho42@gmail.com>
2023-04-01 21:39:27 +0200

    Add if-elif-else chain

---
+++ 4.IF STATEMENT/I.Try IT Four/f_stagesOfLife.py
@@ -0,0 +1,16 @@
+# 4.7 Write an if-elif-else chain that determines a person’s
+# stage of life
+
+age = 5
+if age < 2:
+    print("The person is a baby")
+elif age < 4:
+    print("The person is toddler")
+elif age < 16:
+    print("The person is kid")
+elif age < 20:
+    print("The person is teenager")
+elif age < 65:
+    print("The person is adult")
+else:
+    print("The person is elder")
