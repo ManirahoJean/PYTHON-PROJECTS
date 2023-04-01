ba8d2c38833511543a913220e821be90a2688326
Maniraho <johmaniraho42@gmail.com>
2023-04-01 21:15:14 +0200

    Add if else

---
+++ 4.IF STATEMENT/I.Try IT Four/e_alien_colors3.py
@@ -0,0 +1,10 @@
+# 4.5 turn if-else into if-elif-else chain
+
+alien_color = ['green', 'yellow', 'red']
+
+if 'green' in alien_color:
+    print("You earned 5 points")
+elif 'yellow' in alien_color:
+    print("You earned 10 points")
+else:
+    print("You earned 15 points")
