1d3986a5de22ac80f7100bdd735d6b81be9c2793
Maniraho <johmaniraho42@gmail.com>
2023-04-02 16:17:23 +0200

    Check special item

--- 4.IF STATEMENT/H.If Statement with List/toppings3.py
+++ 4.IF STATEMENT/H.If Statement with List/toppings3.py
@@ -1,1 +1,18 @@
+# Checking for Special Items
 
+requested_toppings = ['mushrooms', 'green peppers', 'extra cheese']
+for requested_topping in requested_toppings:
+    print(f"Add {requested_topping}.")
+print("Finished making your pizza")
+
+print("\n")
+
+# But what if the pizzeria runs out of green peppers?
+
+requested_toppings = ['mushrooms', 'green peppers', 'extra cheese']
+for requested_topping in requested_toppings:
+    if requested_topping == 'green peppers':
+        print("Sorry, we are running out of green peppers")
+    else:
+        print(f"Adding {requested_topping}.")
+print("Finished making your pizza!")
