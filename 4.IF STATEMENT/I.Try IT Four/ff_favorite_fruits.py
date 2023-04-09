1b4595aa9ae0b79d8164befb4f31f75c2bd80764
Maniraho <johmaniraho42@gmail.com>
2023-04-09 14:51:18 +0200

    use if block

---
+++ 4.IF STATEMENT/I.Try IT Four/ff_favorite_fruits.py
@@ -0,0 +1,17 @@
+# Make a list of your favorite fruits, and then write a series of
+# independent if statements that check for certain fruits in your list
+
+#  Make a list of your three favorite fruits and call it favorite_fruits
+
+favorite_fruits = ['banana', 'orange', 'avocado']
+
+if 'banana' in favorite_fruits:
+    print(f"You real like banana")
+if 'orange' in favorite_fruits:
+    print(F"You real like orange")
+if 'avocado' in favorite_fruits:
+    print(f"You real like avocado")
+if 'apple' in favorite_fruits:
+    print(f"You real like apple")
+if 'pineapple' in favorite_fruits:
+    print(f"You real like pineapple")
