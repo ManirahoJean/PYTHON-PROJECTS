5c559728f55f5301e0682bacd361ed88b691ebcf
Maniraho <johmaniraho42@gmail.com>
2023-04-10 14:09:30 +0200

    store information

--- 5.DICTIONARIES/D.TRY IT FIVE/a_person.py
+++ 5.DICTIONARIES/D.TRY IT FIVE/a_person.py
@@ -1,1 +1,9 @@
+# 5.1
+#  Use a dictionary to store information about a person you know
 
+person_id = {'first_name': 'Vincent', 'last_name': 'kira', 'age': 30, 'city': 'kigali'}
+
+print(f"First Name is: {person_id['first_name'].title()}")
+print(f"last name is: {person_id['last_name'].title()}")
+print(f"Age: {person_id['age']}, and the City is")
+print(f"{person_id['city']}")
