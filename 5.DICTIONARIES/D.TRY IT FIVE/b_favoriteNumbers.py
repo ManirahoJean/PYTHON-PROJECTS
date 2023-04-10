54ee5c8beef640fa86e92d8b7850e8f595b2e4ed
Maniraho <johmaniraho42@gmail.com>
2023-04-10 14:08:34 +0200

    Access Values in dictionary

---
+++ 5.DICTIONARIES/D.TRY IT FIVE/b_favoriteNumbers.py
@@ -0,0 +1,8 @@
+# 5. 2  Favorite Numbers
+# Use a dictionary to store people’s favorite numbers
+
+people_numbers = {'emmy': 8, 'vicent': 9, 'willy': 6, 'edward': '5', 'john': 10}
+
+print(f"Name: Emmy, favorite number: {people_numbers['emmy']}")
+print(f"Name: Vincent, favorite number: {people_numbers['vicent']}")
+print(f"Name: willy, favorite number: {people_numbers['willy']}")
---
+++ 5.DICTIONARIES/D.TRY IT FIVE/c_ glossary.py
@@ -0,0 +1,9 @@
+# 5.3  Glossary
+programming_words = {
+    'string': 'series of characters',
+    'comment': 'notes in the program that can not be executed',
+    'print': 'display the output of the program',
+    'list': 'collection items',
+    'dictionary': 'collection of key values'
+}
+print(f"string, is defined as {programming_words['string']}.")
