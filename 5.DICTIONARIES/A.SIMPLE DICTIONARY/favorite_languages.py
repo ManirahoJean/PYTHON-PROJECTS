b716ecfdfc919ed8c20568e81b2c24ca7719a4a0
Maniraho <johmaniraho42@gmail.com>
2023-04-11 22:09:12 +0200

    Dictionary of similar object

---
+++ 5.DICTIONARIES/A.SIMPLE DICTIONARY/favorite_languages.py
@@ -0,0 +1,11 @@
+# A Dictionary of Similar Objects
+
+favorite_languages = {
+    'jane': 'python',
+    'sarah': 'c',
+    'edward': 'ruby',
+    'phil': 'python'
+}
+language = favorite_languages['sarah'].title()
+print(f"Sarah's favorite language is {language}")
+print(f"Edward's favorite language is {favorite_languages['edward'].title()}")
