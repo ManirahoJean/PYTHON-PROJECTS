6339caf85a3ded616aef2008ebfdfd130e3a7af8
Maniraho <johmaniraho42@gmail.com>
2023-04-11 22:16:43 +0200

    Loop Through All Key-Value Pairs

--- 5.DICTIONARIES/B.LOOPING THROUGH DICT/user.py
+++ 5.DICTIONARIES/B.LOOPING THROUGH DICT/user.py
@@ -1,2 +1,13 @@
 # Looping Through All Key-Value Pairs
 
+user_o = {
+    'username': 'efermi',
+    'first': 'enrico',
+    'last': 'fermi'
+}
+for key, value in user_o.items():
+    print(f"\nkey: {key}")
+    print(f"value: {value}")
+
+
+
