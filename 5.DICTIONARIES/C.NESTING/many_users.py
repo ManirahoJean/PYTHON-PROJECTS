ed56d476ebaf43e1a0c8367ff9e3bc7935c3bbde
Maniraho <johmaniraho42@gmail.com>
2023-04-16 10:36:01 +0200

    A Dictionary in a Dictionary

---
+++ 5.DICTIONARIES/C.NESTING/many_users.py
@@ -0,0 +1,26 @@
+# A Dictionary in a Dictionary
+
+users = {
+    'aeinstein': {
+        'first': 'einstein',
+        'last': 'albert',
+        'location': 'princeton'
+    },
+    'mcurie': {
+        'first': 'marie',
+        'last': 'curie',
+        'location': 'paris'
+    }
+}
+
+for username, user_info in users.items():
+    print(f"Username: {username}")
+    full_name = f"{user_info['first']} + {user_info['last']}"
+    location = user_info['location']
+
+    print(f"\tFull Name:{full_name.title()}")
+    print(f"\tLocation: {location.title()}")
+
+
+
+
