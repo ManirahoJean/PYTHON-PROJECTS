332c77f3967577bf31735c77b03b01923d1d4891
Maniraho <johmaniraho42@gmail.com>
2023-04-16 21:31:40 +0200

    Input function

--- 6.USER INPUT AND WHILE LOOPS/A.How Input Funct Works/parrots.py
+++ 6.USER INPUT AND WHILE LOOPS/A.How Input Funct Works/parrots.py
@@ -1,1 +1,31 @@
+# The input() function pauses your program and waits for the user to enter
+# some text
+
+"""""
+message = input("Tell me something, I will repeat it back to you: ")
+print(message)
+
+"""""
+# Writing Clear Prompts
+
+"""""
+name = input("Please enter your name: ")
+print(f"\nHello {name.title()}!")
+
+"""""
+
+# Sometimes you’ll want to write a prompt that’s longer than one line
+
+prompt = "If You tell us who you are, we can personalize the message you see"
+prompt += "What is your first name? "
+
+name = input(prompt)
+print(f"\nHello, {name}!")
+
+# When you use the input() function, Python interprets everything the user
+# enters as a string
+
+
+
+
 
