727138ea1a88d3f74d29740d78b63d604a6c20db
Maniraho <johmaniraho42@gmail.com>
2023-04-16 21:41:14 +0200

    Accept Numerical Input

---
+++ 6.USER INPUT AND WHILE LOOPS/A.How Input Funct Works/rollercoaster.py
@@ -0,0 +1,10 @@
+# Using int() to Accept Numerical Input
+
+height = input("How tall are you, in inches?")
+height = int(height)
+
+if height >= 48:
+    print("You are tall enough to ride")
+else:
+    print("You'll be able to ride when you are little bit older")
+
