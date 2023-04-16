0f0b965b4a1b4d92c088b9fe4ae7a8d13d25d3af
Maniraho <johmaniraho42@gmail.com>
2023-04-16 22:06:21 +0200

    Check if input is odd or even number

---
+++ 6.USER INPUT AND WHILE LOOPS/A.How Input Funct Works/even_or_odd.py
@@ -0,0 +1,8 @@
+
+number = input("Enter a number, I will tell you if is odd or even: ")
+number = int(number)
+
+if number % 2 == 0:
+    print(f"\n {number} is an even number!")
+else:
+    print(f"{number} is an odd number")
