b8bbb4f6f537aad901613d222395b46423422bad
Maniraho <johmaniraho42@gmail.com>
2023-04-17 07:59:15 +0200

    Count using while loop

--- 6.USER INPUT AND WHILE LOOPS/B.Introducing While Loops/counting.py
+++ 6.USER INPUT AND WHILE LOOPS/B.Introducing While Loops/counting.py
@@ -1,1 +1,9 @@
+# The for loop takes a collection of items and executes a block of code once
+# for each item in the collection. In contrast, the while loop runs as long as,
+# or while, a certain condition is true.
+
+current_number = 1
+while current_number <= 5:
+    print(current_number)
+    current_number += 1
 
