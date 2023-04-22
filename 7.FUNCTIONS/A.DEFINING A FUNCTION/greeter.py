f96edeaa47d1a4c898a278c94e447420989f3567
Maniraho <johmaniraho42@gmail.com>
2023-04-22 08:33:21 +0200

    Pass Information to a Function

--- 7.FUNCTIONS/A.DEFINING A FUNCTION/greeter.py
+++ 7.FUNCTIONS/A.DEFINING A FUNCTION/greeter.py
@@ -1,1 +1,23 @@
+# functions are named blocks of code that are designed
+# to do one specific job. When you want to perform a particular task
+# that you’ve defined in a function, you call the function
+# responsible for it
+
+def greet_user():
+    """ Display the message """
+    print("Hello World!")
+
+
+greet_user()
+
+# Passing Information to a Function
+
+
+def greet_user(username):
+
+    """ Display greeting """
+    print(f"Hello {username}.")
+
+
+greet_user('jessie')
 
