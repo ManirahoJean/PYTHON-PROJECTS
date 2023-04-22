f7b793a04db59ca040d53eb5079189e2711a6656
Maniraho <johmaniraho42@gmail.com>
2023-04-22 08:47:15 +0200

    Favorite book

---
+++ 7.FUNCTIONS/F.TRY IT SEVEN/b_ favoriteBook.py
@@ -0,0 +1,16 @@
+# 7.2 Favorite book
+
+"""
+Write a function called favorite_book() that accepts one
+parameter, title. The function should print a message, such as One of my
+favorite books is Alice in Wonderland. Call the function, making sure to
+include a book title as an argument in the function call.
+"""
+
+
+def favorite_book(book):
+    """ Display book's title """
+    print(f"My favorite book is {book}.")
+
+
+favorite_book("Rich Dad Poor Dad")
