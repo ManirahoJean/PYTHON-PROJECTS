721d9469a76658fd105bf2ea5cb2be4b0cfef4d4
Maniraho <johmaniraho42@gmail.com>
2023-04-22 08:41:41 +0200

    Display message

--- 7.FUNCTIONS/F.TRY IT SEVEN/a_message.py
+++ 7.FUNCTIONS/F.TRY IT SEVEN/a_message.py
@@ -1,1 +1,17 @@
+# 7-1. Message
+
+"""
+Write a function called display_message() that prints one
+sentence telling everyone what you are learning about in this chapter.
+Call the function, and make sure the message displays correctly
+
+"""
+
+
+def display_message():
+    """Display message"""
+    print("Hi there, I'm learning how to define a function in python.")
+
+
+display_message()
 
