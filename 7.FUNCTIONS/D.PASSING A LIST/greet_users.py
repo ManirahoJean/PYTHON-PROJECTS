0d54f45dc3a4fb82c95b236574839b5b5cc66951
Maniraho <johmaniraho42@gmail.com>
2023-04-27 20:45:03 +0200

    Passing a list to function

--- 7.FUNCTIONS/D.PASSING A LIST/greet_users.py
+++ 7.FUNCTIONS/D.PASSING A LIST/greet_users.py
@@ -1,1 +1,15 @@
+# Passing A List
 
+# You’ll often find it useful to pass a list to a function, whether
+# it’s a list of  names, numbers, or more complex objects, such as dictionaries
+
+def greet_users(names):
+    """Print a simple greeting to each user in te list"""
+    for name in names:
+        msg = f"Hello, {name.title()}"
+        print(msg)
+
+
+usernames = ['emmy', 'nicky', 'jd']
+
+greet_users(usernames)
