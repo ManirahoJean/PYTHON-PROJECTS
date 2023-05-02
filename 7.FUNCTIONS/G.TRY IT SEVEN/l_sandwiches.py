e781f3ca96a33ba11cc1dbc540d7ed44ed3129bd
Maniraho <johmaniraho42@gmail.com>
2023-05-02 22:01:28 +0300

    Sandwiches

---
+++ 7.FUNCTIONS/G.TRY IT SEVEN/l_sandwiches.py
@@ -0,0 +1,20 @@
+# Sandwiches
+"""
+7-12. Sandwiches: Write a function that accepts a list of items a person wants
+on a sandwich. The function should have one parameter that collects as many
+items as the function call provides, and it should print a summary of the sandwich
+ that’s being ordered. Call the function three times, using a different number
+of arguments each time.
+
+"""
+
+
+def make_sandwich(*sandwiches):
+    """ Make list of items on a sandwich"""
+    print("add those items to the sandwich:")
+    for sandwich in sandwiches:
+        print(f"- {sandwich}")
+
+
+make_sandwich('meat')
+make_sandwich('meat', 'vegetables', 'malt')
