bc8e0ef4c13813f43b45c1d37fb412ffd2bed501
Maniraho <johmaniraho42@gmail.com>
2023-05-03 09:37:35 +0300

    Use Arbitrary Keyword Argument

--- 7.FUNCTIONS/E.PASSING AN ARBITRARY NBER OF ARG/user_profile.py
+++ 7.FUNCTIONS/E.PASSING AN ARBITRARY NBER OF ARG/user_profile.py
@@ -11,12 +11,36 @@ pairs as the calling statement provides
 
 def build_profile(first, last, **user_info):
     """ Build a dictionary containing everything we know about the user """
-    user_info['first_name'] = first
-    user_info['last_name'] = last
+    user_info['first name'] = first
+    user_info['last name'] = last
     return user_info
 
 
 user_profile = build_profile('albert', 'einstein',
                              location='princeton',
                              field='physics')
+print(user_profile)
+
+print("\n")
+
+# using other way for making a profile dictionary
+
+
+def build_profile(first, last, **user_info):
+    """ Build a dictionary containing everything about the user"""
+    profile_dict = {
+        'first name': first.title(),
+        'last name': last.title()
+    }
+
+    for address, name in user_info.items():
+        """ Save the address and name in the dictionary """
+        profile_dict[address] = name
+    return profile_dict
+
+
+user_profile = build_profile('mani', 'jd',
+                             location='kigali',
+                             field='coding',
+                             age=30)
 print(user_profile)
