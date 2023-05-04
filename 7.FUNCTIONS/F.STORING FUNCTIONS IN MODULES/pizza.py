9a8a417125702add174081527da3944ba59b0f2b
Maniraho <johmaniraho42@gmail.com>
2023-05-04 09:04:13 +0300

    Import module

--- 7.FUNCTIONS/F.STORING FUNCTIONS IN MODULES/pizza.py
+++ 7.FUNCTIONS/F.STORING FUNCTIONS IN MODULES/pizza.py
@@ -1,1 +1,14 @@
+# Importing an Entire Module
+"""
+Let’s make a module that contains the function make_pizza(). To
+make this module, we’ll remove everything from the file pizza.py except the
+function make_pizza():
 
+"""
+
+
+def make_pizza(size, *toppings):
+    """ Summarize pizza we're about to make """
+    print(f"Making a {size}-inch pizza with the following toppings: ")
+    for topping in toppings:
+        print(f"-{topping}")
