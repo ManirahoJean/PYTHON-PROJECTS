9791b81fa69d98fa6a412777d6c4703dc081d7e5
Maniraho <johmaniraho42@gmail.com>
2023-05-10 08:39:21 +0300

    Restaurant

--- 8.CLASSES/5.TRY IT EIGHT/a_restaurant.py
+++ 8.CLASSES/5.TRY IT EIGHT/a_restaurant.py
@@ -1,1 +1,33 @@
+# 8-1. Restaurant
 
+"""
+8-1. Restaurant: Make a class called Restaurant. The __init__() method for
+Restaurant should store two attributes: a restaurant_name and a cuisine_type.
+Make a method called describe_restaurant() that prints these two pieces of
+information, and a method called open_restaurant() that prints a message indicating
+that the restaurant is open.Make an instance called restaurant from your class.
+Print the two attributes individually, and then call both methods
+
+"""
+
+
+class Restaurant:
+    """ A simple attempt to model a restaurant """
+    def __init__(self, restaurant_name, cuisine_type):
+        self.restaurant_name = restaurant_name
+        self.cuisine_type = cuisine_type
+
+    def describe_restaurant(self):
+        print(f"In {self.restaurant_name}, everyone is welcome!")
+        print(f"{self.cuisine_type} is our foods")
+
+    def open_restaurant(self):
+        print(f"{self.restaurant_name} is now open.")
+
+
+restaurant = Restaurant('Happy Day', 'Traditional foods')
+print(f"Our restaurant's name is {restaurant.restaurant_name}")
+print(f"Type of cuisine: {restaurant.cuisine_type}.")
+
+restaurant.describe_restaurant()
+restaurant.open_restaurant()
